00412e68cb8e4b3eef825f5a389c7d91ce539317
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-10-15 17:04:38 +0200

    image avatar modifié + html&JS linké + petites modifs sur HTML + première page de JS

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("start-button").addEventListener("click", function () {
+	const mainSection = document.querySelector(".carte");
+	mainSection.innerHTML = "";
+
+	const avatarImg = document.createElement("img");
+	avatarImg.src = "/images/avatar.png";
+	avatarImg.alt = "Avatar";
+	avatarImg.classList.add("avatar");
+	mainSection.appendChild(avatarImg);
+
+	const authentication = document.createElement("h2");
+	authentication.textContent = "Enter your name";
+	mainSection.appendChild(authentication);
+
+	const nameInput = document.createElement("input");
+	nameInput.type = "text";
+	nameInput.id = "player-name";
+	nameInput.placeholder = "Nadarkhily Wild";
+	mainSection.appendChild(nameInput);
+
+	const readyButton = document.createElement("button");
+	readyButton.textContent = "Ready !";
+	readyButton.id = "ready-button";
+	mainSection.appendChild(readyButton);
+});
