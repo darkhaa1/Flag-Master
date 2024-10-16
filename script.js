3f8f83503f42499e86a8db75b760d5a76c2bbddb
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-10-16 15:04:28 +0200

    remis modification

--- script.js
+++ script.js
@@ -27,17 +27,62 @@ document.getElementById("start-button").addEventListener("click", () => {
 
 	const readyButton = document.createElement("button");
 	readyButton.textContent = "Ready !";
-	readyButton.id = "start-button";
+	readyButton.classList.add("readyButton");
 	mainSection.appendChild(readyButton);
+
+	readyButton.addEventListener("click", startQuiz);
 });
+
+let currentQuestionIndex = 0;
+
+function startQuiz() {
+	const playerName = document.getElementById("player-name").value;
+	if (!playerName) {
+		alert("Enter your name before starting");
+		return;
+	}
+	document.querySelector(".carte").innerHTML = "";
+	createQuestion();
+}
 
 const questions = [
 	{
-		title: "Where does this flag fly",
-		picture: "images/Flag_of_Madgascar.svg",
+		question: "Where does this flag fly",
+		picture: "images/Flag_of_Madagascar.svg",
+		options: ["France", "Mongolia", "Poland", "Madagscar"],
+		correct: "Madagscar",
+	},
+	{
+		question: "Where does this flag fly",
+		picture: "images/france.svg",
+		options: ["France", "Mongolia", "Poland", "Madagscar"],
+		correct: "France",
 	},
 ];
 
-//    readyButton.addEventListener("click", () => {
+function createQuestion() {
+	const mainSection = document.querySelector(".carte");
 
-//    })
+	const currentQuestion = questions[currentQuestionIndex];
+
+	const imgFlag = document.createElement("img");
+	imgFlag.src = currentQuestion.picture;
+	imgFlag.alt = "flag";
+	imgFlag.classList.add("flag-image");
+	imgFlag.style.height = "12em";
+	mainSection.appendChild(imgFlag);
+
+	const question = document.createElement("h3");
+	question.textContent = currentQuestion.question;
+	mainSection.appendChild(question);
+
+	currentQuestion.options.forEach((option) => {
+		const optionButton = document.createElement("button");
+		optionButton.textContent = option;
+		optionButton.classList.add("option-button");
+		optionButton.addEventListener("click", () => checkAnswer(option));
+		optionButton;
+		mainSection.appendChild(optionButton);
+	});
+}
+console.log;
