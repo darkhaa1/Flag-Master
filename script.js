5dd298afdaababc6c0e5b782b50af9a97e099b28
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-10-16 10:22:28 +0200

    certain style sur js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.getElementById("start-button").addEventListener("click", () => {
 	const authentication = document.createElement("h2");
 	authentication.textContent = "Enter your name";
 	mainSection.appendChild(authentication);
+	authentication.style.paddingBottom = "0";
 
 	const nameInput = document.createElement("input");
 	nameInput.type = "text";
@@ -19,9 +20,24 @@ document.getElementById("start-button").addEventListener("click", () => {
 	mainSection.appendChild(nameInput);
 	nameInput.style.height = "4em";
 	nameInput.style.width = "20em";
+	nameInput.style.borderRadius = "1vw";
+	nameInput.style.border = "none";
+	nameInput.style.paddingLeft = "2vw";
+	nameInput.style.paddingRight = "2vw";
 
 	const readyButton = document.createElement("button");
 	readyButton.textContent = "Ready !";
 	readyButton.id = "start-button";
 	mainSection.appendChild(readyButton);
 });
+
+const questions = [
+	{
+		title: "Where does this flag fly",
+		picture: "images/Flag_of_Madgascar.svg",
+	},
+];
+
+//    readyButton.addEventListener("click", () => {
+
+//    })
